e40b0a29b40f09aa0a3b738c29f26422e44eb362
Hiba <hiba.hf123@gmail.com>
2021-06-25 11:32:21 +0530

    changed img tag to image in index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import { getSortedPostsData } from '../lib/posts'
 import Navbar from '../components/navbar/index'
 import Header from '../components/Header/index'
-
 import styles from '../styles/index.module.scss'
-import { steelblue } from 'color-name'
+import WordsSVG from '../public/images/Words.svg'
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,7 +13,8 @@ export default function Home({ allPostsData }) {
       <Navbar exp='active'/>
       <Header title="Expression" image="/images/expression.svg" sec1="Words" sec2="Strokes" s1="active"/>
       <section className={styles.container}>
-        <img src="/images/words.svg" alt="Words" />
+      <div className={styles.pic}>
+        <Image src={WordsSVG} alt="Words"  height={300} width={1000}  /> </div>
         <div className={styles.blogposts}>
           {allPostsData.map(({ id, date, title, preview, category }) => (
             <div className={styles.blogCard} key={id} >
