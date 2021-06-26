1a633dfdbb121720847c3e61ea8e2eadc707bef2
Hiba <hiba.hf123@gmail.com>
2021-06-26 14:00:12 +0530

    fixed font sizes

--- components/about/index.js
+++ components/about/index.js
@@ -3,7 +3,6 @@ import styles from "./styles.module.scss";
 export default function About(){
     return(
         <div className={styles.container}>
-            <p className={styles.title}>About</p>
             <div className={styles.content}>
                 <p>{`Hey, this is Hiba! 'Almost' is very close to my heart; a project I've been meaning to do for years now. There's probably no other word I relate to as much as 'almost'. I'm an almost engineer, an almost writer, an almost designer, an almost lover, an almost everything; not quite, but very nearly though. It's a daunting struggle to not be able to cross the 'almost' benchmark in almost all aspects of life. It's an even bigger struggle to embrace all the 'almost' pieces of you. It's a confusing battle between accepting and pushing oneself harder. 
                 Everything out here in this tiny little corner of the internet is a self-attempt at this super confusing battle. I often see myself as an exaggeration of confusion brewing inside this human frame. Over the years, I’ve learned that perhaps confusion is a permanent state of our minds. This little virtual almost rant room of mine is just an extension of this confusion. `}
