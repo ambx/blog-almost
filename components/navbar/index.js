030fda0499cad8b7fd6e71d2da198b9a673d6bea
Hiba <hiba.hf123@gmail.com>
2021-06-26 14:12:57 +0530

    added logo

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Logo from '/public/images/logo.js'
 import styles from "./styles.module.scss"
 
 export default function navbar(props) {
   return (
     <nav className={styles.NavbarItem}>
-      <div className={styles.navLogo}><li><Link href="/"><a className={props.exp}>Almost</a></Link></li></div>
+      <div className={styles.navLogo}><li><Link href="/"><a className={props.exp}><Logo /></a></Link></li></div>
       <ul> 
        
       </ul>
