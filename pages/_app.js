54268f64dcf004457afdebc1e210b5b3ec09f08e
ambx <hibaf.me18@sahyadri.edu.in>
2022-04-26 04:55:14 +0530

    typography update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import Layout from '../components/Layout/index'
 import '../styles/globals.scss'
+import "@fontsource/inter"
+import "@fontsource/raleway/200.css"
+import "@fontsource/raleway/700.css"
+import "@fontsource/montserrat"
 
 export default function MyApp({ Component, pageProps }) {
   return (
